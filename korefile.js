c9c28a21a1a0d0a25574e3aa80168555fc513242
Robert Konrad <robert@ktx-software.com>
2014-09-04 03:40:40 +0200

    Created korefile for libgit2.

---
+++ korefile.js
@@ -0,0 +1,42 @@
+var solution = new Solution('gittest');
+var project = new Project('gittest');
+
+solution.setCmd();
+
+project.addExclude('.git/**');
+project.addExclude('libgit2/.git/**');
+project.addExclude('build/**');
+
+project.addFile('Sources/**');
+
+function addLibFiles() {
+	for (var i = 0; i < arguments.length; ++i) {
+		project.addFile('libgit2/' + arguments[i]);
+	}
+}
+
+addLibFiles('src/*.c', 'src/*.h', 'src/transports/*.c', 'src/transports/*.h', 'src/xdiff/*.c', 'src/xdiff/*.h');
+addLibFiles('src/win32/*.c', 'src/win32/*.h');
+//project.addFiles('src/unix/*.c', 'src/unix/*.h');
+
+project.addDefines('WIN32', '_WIN32_WINNT=0x0501');
+
+project.addIncludeDirs('libgit2/src', 'libgit2/include');
+
+project.addDefine('GIT_WINHTTP');
+project.addIncludeDir('libgit2/deps/http-parser');
+addLibFiles('deps/http-parser/*.c', 'deps/http-parser/*.h');
+
+project.addDefine('WIN32_SHA1');
+addLibFiles('src/hash/hash_win32.c');
+
+project.addIncludeDir('libgit2/deps/regex');
+addLibFiles('deps/regex/regex.c');
+
+project.addIncludeDir('libgit2/deps/zlib');
+project.addDefines('NO_VIZ', 'STDC', 'NO_GZIP');
+addLibFiles('deps/zlib/*.c', 'deps/zlib/*.h');
+
+solution.addProject(project);
+
+return solution;
