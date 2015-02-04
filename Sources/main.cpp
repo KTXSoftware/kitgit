2a03bab1e2e85edb656b5b339bd814f253c6b71b
Robert Konrad <robert@ktx-software.com>
2015-02-04 15:32:45 +0100

    Commit successful merges.

--- Sources/main.cpp
+++ Sources/main.cpp
@@ -69,14 +69,14 @@ void check_lg2(int error, const char *message, const char *extra) {
 	exit(1);
 }
 
-int pull() {
+int pull(const char* path) {
 	git_repository* repo;
 	git_reference* current_branch;
 	git_reference* upstream;
 	git_buf remote_name = { 0 };
 	git_remote* remote;
 	
-	check_lg2(git_repository_open_ext(&repo, "kraffiti", 0, NULL), "failed to open repo", NULL);
+	check_lg2(git_repository_open_ext(&repo, path, 0, NULL), "failed to open repo", NULL);
 	check_lg2(git_repository_head(&current_branch, repo), "failed to lookup current branch", NULL);
 	check_lg2(git_branch_upstream(&upstream, current_branch), "failed to get upstream branch", NULL);
 	check_lg2(git_branch_remote_name(&remote_name, repo, git_reference_name(upstream)), "failed to get the reference's upstream", NULL);
@@ -122,10 +122,31 @@ int pull() {
 				git_index_free(index);
 				if (has_conflicts) {
 					printf("There were conflicts merging. Please resolve them and commit.\n");
-					git_reference_free(upstream);
-					git_reference_free(current_branch);
-					git_repository_free(repo);
-					return 0;
+				}
+				else {
+					git_index *index;
+					git_buf message = { 0 };
+					git_oid commit_id, tree_id;
+					git_commit *parents[2];
+					git_signature *user;
+					git_tree *tree;
+					
+					check_lg2(git_repository_index(&index, repo), "failed to load index", NULL);
+					check_lg2(git_index_write_tree(&tree_id, index), "failed to write tree", NULL);
+					git_index_free(index);
+					
+					check_lg2(git_signature_default(&user, repo), "failed to get user's ident", NULL);
+					check_lg2(git_repository_message(&message, repo), "failed to get message", NULL);
+					
+					check_lg2(git_tree_lookup(&tree, repo, &tree_id), "failed to lookup tree", NULL);
+					
+					check_lg2(git_commit_lookup(&parents[0], repo, git_reference_target(current_branch)), "failed to lookup first parent", NULL);
+					check_lg2(git_commit_lookup(&parents[1], repo, git_reference_target(upstream)), "failed to lookup second parent", NULL);
+
+					check_lg2(git_commit_create(&commit_id, repo, "HEAD", user, user, NULL, message.ptr, tree, 2, (const git_commit **)parents), "failed to create commit", NULL);
+
+					git_tree_free(tree);
+					git_signature_free(user);
 				}
 			}
 		}
@@ -142,14 +163,12 @@ int pull() {
 	return 0;
 }
 
-void clone() {
+void clone(const char* url, const char* path) {
 	git_repository* repo = NULL;
 	git_clone_options options = GIT_CLONE_OPTIONS_INIT;
 	options.remote_callbacks.transfer_progress = transfer_progress;
 
-	name = "kraffiti";
-	basePath = "kraffiti";
-	const char* url = "https://github.com/ktxsoftware/kraffiti.git";
+	basePath = path;
 	int index = lastIndexOf(url, '/');
 	strncpy(baseUrl, url, lastIndexOf(url, '/') + 1);
 	git_clone(&repo, url, basePath, &options);
@@ -158,6 +177,8 @@ void clone() {
 }
 
 int main(int argc, char** argv) {
-	//clone();
-	pull();
+	const char* path = "kraffiti";
+	const char* url = "https://github.com/ktxsoftware/kraffiti.git";
+	clone(url, path);
+	pull(path);
 }
